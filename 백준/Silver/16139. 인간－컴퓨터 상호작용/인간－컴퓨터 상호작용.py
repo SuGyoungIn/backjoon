f1c2af9782d1c7c09ed6567f5a363e08a7494c54
Indudu <88818113+SuGyoungIn@users.noreply.github.com>
2023-10-08 21:44:34 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 440 ms, Memory: 176848 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -3,17 +3,28 @@ input = sys.stdin.readline
 
 S = input().rstrip()
 T = int(input())
+
+n = len(S)
+alpha = [[0]*n for _ in range(26)]
+
+alpha[ord(S[0])-97][0] += 1
+
+for j in range(1,n):
+    for i in range(26):
+        if ord(S[j]) - 97 == i:
+            alpha[i][j] = alpha[i][j-1] + 1
+        else:
+            alpha[i][j] = alpha[i][j-1]
+
 ans = []
 for _ in range(T):
     q = list(map(str,input().rstrip().split()))
-    a = q[0]
-    l = int(q[1])
-    r = int(q[2])
-    cnt = 0
-    for i in range(l,r+1):
-        if S[i] == a:
-            cnt += 1
+    a,l,r = q[0],int(q[1]),int(q[2])
+    idx = ord(a)-97
 
-    ans.append(cnt)
+    if l == 0:
+        ans.append(alpha[idx][r])
+    else:
+        ans.append(alpha[idx][r] - alpha[idx][l-1])
 
 print(*ans,sep='\n')
